78902f7c31b093770317ca5eabfd93e0de36a423
cowld <cowldnd2@naver.com>
2022-11-25 09:36:13 +0900

    Initial

---
+++ src/main/java/com/project/kostyle/KostyleApplication.java
@@ -0,0 +1,13 @@
+package com.project.kostyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KostyleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/kostyle/KostyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kostyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KostyleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
