3224d51e2bb9de5bfae3c1d78dcac061c4ee24d2
cowld <cowldnd2@naver.com>
2022-11-28 10:33:36 +0900

    11/28 cjw 상품 crud 및 조회수 증가 구현

--- src/main/java/com/project/kostyle/service/ProductService.java
+++ src/main/java/com/project/kostyle/service/ProductService.java
@@ -5,6 +5,8 @@ import com.project.kostyle.dto.product.SearchDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface ProductService {
@@ -12,4 +14,10 @@ public interface ProductService {
     Long create(ProductDto dto);
 
     Page<ProductDto> findAll(Pageable pageable, SearchDto search);
+
+    ProductDto findOne(Long pno, Cookie cookie, HttpServletResponse response);
+
+    Long update(ProductDto dto);
+
+    Long delete(Long pno);
 }
